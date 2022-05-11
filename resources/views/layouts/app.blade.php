017ca33039981f792630a06b8c683d8f296d5244
Lab-dev-one <pablo1532008@gmail.com>
2022-05-11 17:06:12 -0300

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
+        <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.css">
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+<link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.css">
 <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -17,10 +18,14 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <h1 class="text-xl">Welcome, <span class="font-bold" style="font-weight:bold;">{{$nome[0]}}</span></h1>
-                    <h1><span class="font-extrabold">Email:</span> {{Auth::user()->email}}</h1>
-                    <h1><span class="font-semibold">Phone:</span> {{Auth::user()->phone}}</h1>
+                    <h1><span class="font-extrabold" style="font-weight:bold;">Email:</span> {{Auth::user()->email}}</h1>
+                    <h1><span class="font-extrabold" style="font-weight:bold;">Phone:</span> {{Auth::user()->phone}}</h1>
                     <h1></h1>
                     
+                    <div class="alert alert-dark" role="alert">
+                        A simple dark alert—check it out!
+                    </div>
+
                 </div>
             </div>
         </div>
